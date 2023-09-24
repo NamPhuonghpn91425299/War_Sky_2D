a90bd279213f6b1196c40e4c00dc3680cf7dccd4
NamPhuonghpn91425299 <115439386+NamPhuonghpn91425299@users.noreply.github.com>
2023-09-24 16:31:31 +0700

    update code move

---
+++ Assets/_Data/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+    public static InputManager Instance { get => instance;}
+    [SerializeField] protected Vector3 mouseWorldPos;
+    public Vector3 MouseWorldPos { get => mouseWorldPos; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (InputManager.instance != null) Debug.LogError("Chỉ được dùng 1 singleton");
+        InputManager.instance = this;
+    }
+    void FixedUpdate()
+    {
+        this.GetMouse();
+    }
+    protected virtual void GetMouse()
+    {
+        this.mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+}
